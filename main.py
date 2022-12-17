bb81d3cb7c57ff2e34560ee4ad0132104c24ac94
Bhavin Thakur <77884771+developer1029@users.noreply.github.com>
2022-12-17 23:46:44 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+
+
+  #----------importing time modules----------#
+import time
+
+  #----------assigning variables----------#
+secs = 10 * int(input('Enter seconds: '))
+
+  #----------creating loop----------#
+for part_sec in range(secs-1,-1,-1):
+  time.sleep(0.1)
+  #----------calculations----------#
+  seconds = part_sec // 10
+  format_min = seconds // 60
+  format_sec = seconds % 60
+  format_milli = (part_sec % 10) * 100
+  #----------print statement for the loop----------#
+  print(f"\t{format_min:02d}m:{format_sec:02d}s:{format_milli}ms\r", end='')
+  
+  #----------final print statement----------#
+print('\n\nTime\'s up!')
